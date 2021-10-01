46a59c5f54495b8e34a33f14a97d8765e87d6901
dwwmmike <dwwmmike@gmail.com>
2021-10-01 12:04:17 +0200

    boucle type done

--- pokedex_1.0/src/PostList.js
+++ pokedex_1.0/src/PostList.js
@@ -2,6 +2,24 @@ import React, {Component} from 'react';
 import PostData from './sources/data/List.json';
 
 class PostList extends Component {
+  getType = (type) => { 
+  var pill;
+  switch (type) {
+    case 'Feu':
+      pill="bg-danger";
+      break;
+    case 'Eau':
+      pill="bg-primary";
+      break;
+    case 'Plante':
+      pill="bg-success";
+      break;
+    default:
+      pill="bg-warning";;
+  }
+    return pill;
+  }
+
   render(){
     return(
       <>
@@ -12,11 +30,17 @@ class PostList extends Component {
             <div className="card">
             <div className="card-body">
             <img src={postDetail.photo} className="card-img-top" alt="Image de Pokemon"/>
-              <h5 className="card-title">{postDetail.nom}</h5>
+            <h5 className="card-title">N°{postDetail.id}</h5>
+              <h4 className="card-title">{postDetail.nom}</h4>
               <p className="card-text">{postDetail.desc}</p>
               <hr/>
-              <p>Types: {postDetail.type}</p>
-              <p>Faiblesses: {postDetail.faiblesses}</p>
+              Type: {postDetail.type.map((type,i)=>{
+                var pill = this.getType(type);
+                return(
+                <span className={"badge rounded-pill "+ pill}> {type}</span>
+                )
+              })}
+              <p>Faiblesses: {postDetail.faiblesses.join(" ")}</p>
               <a href="#" className="btn btn-primary">Ajouter</a>
             </div>
             </div>
--- pokedex_1.0/src/Search.js
+++ pokedex_1.0/src/Search.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
+
 const Search = (props)=> {
     const {recup, requete, req} = props;
       return(
         <>
-    <form className="d-flex" onSubmit={(e)=>{e.preventDefault();requete()}}>
+    <form className="d-flex" method="GET">
         <label htmlFor="header-search">
             <span className="visually-hidden">Search</span>
         </label>
         <input
             className="form-control me-2"
-            type="text" onChange={recup}  value={req} className="form-control" placeholder="Search"
+            type="text" onChange={recup}  name="r" value={req} className="form-control" placeholder="Search"
         />
         <button className="btn btn-outline-success" type="submit">Search</button>
     </form>
